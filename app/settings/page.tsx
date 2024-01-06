0a72ab24eb33debe875aeeb395816fe4a835db86
senne <sennebels@gmail.com>
2024-01-06 09:55:18 +0100

    feat: :sparkles: created settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useState } from "react";
+import { fetchGetJSON } from "../helpers/api_helpers";
+
+export default function Settings() {
+  const [loading, setLoading] = useState(false);
+  const fetchAllPets = () => {
+    setLoading(true);
+    console.log("Fetching all pets...");
+    fetchGetJSON("/api/setup")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setLoading(false);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {loading && (
+        <img src="/cat-loading.png" className="h-40 w-40" alt="Loading" />
+      )}
+      <button
+        onClick={() => {
+          fetchAllPets();
+        }}
+        className="w-full h-10 rounded-3xl shadow bg-light-primary text-light-dark font-bold hover:bg-light-primary-2 px-4 py-2"
+      >
+        Fetch all pets
+      </button>
+    </div>
+  );
+}
