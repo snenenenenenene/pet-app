b8c849d0c44eb92ceecfaf6481d24b1c35940fa5
senne <sennebels@gmail.com>
2024-01-08 00:14:26 +0100

    feat: :sparkles: added username to settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -19,6 +19,13 @@ export default function Settings() {
   };
   return (
     <div className="flex flex-col justify-center items-center">
+      <section className="flex mb-4">
+        <picture className="w-20 h-20 rounded-full bg-light-primary"></picture>
+        <span className="flex flex-col justify-center ml-4">
+          <h1 className="text-3xl font-bold">{PB.authStore?.model?.name}</h1>
+          <p className="">{PB.authStore?.model?.email}</p>
+        </span>
+      </section>
       {PB.authStore?.model?.role === "admin" && (
         <>
           {loading && (
